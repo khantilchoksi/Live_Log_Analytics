3328779db231db9fa39acc472a7b991693aeb94f
rshah9 <rshah9@ncsu.edu>
2018-10-28 12:14:48 -0400

    Data cleaning in progress

---
+++ datagenerator/service.py
@@ -0,0 +1,29 @@
+import boto3, csv, random
+import conf as c
+
+client = boto3.client('kinesis', region_name='us-east-1')
+
+with open(c.datafile) as csvfile:
+    reader = csv.DictReader(csvfile)
+    raw_records = list(reader)
+
+# print(raw_records[:5])
+clean_records = []
+for record in raw_records:
+    curr_record = list(record.values())
+    curr_record[1] = curr_record[1][1:]
+    cleaned_endpoint = curr_record[2].split(".")[0]
+    cleaned_endpoint = cleaned_endpoint.split("/")[-1]
+    curr_record[2] = cleaned_endpoint
+    curr_record.append(random.uniform(c.min_delay_response, c.max_delay_response))
+    curr_record[-1] = round(curr_record[-1], 2)
+    clean_records.append(curr_record)
+
+print([random.choice(clean_records) for i in range(10)])
+
+start_point = random.randint(0, len(clean_records)-1)
+
+while True:
+
+    time.sleep(random.uniform(c.min_delay_record, c.max_delay_record))
+
