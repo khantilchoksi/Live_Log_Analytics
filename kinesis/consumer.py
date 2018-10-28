f6c41e5741e77b8fb8c85c69840133ea9bb8523c
rshah9 <rshah9@ncsu.edu>
2018-10-28 15:04:24 -0400

    restructure and unification

---
+++ kinesis/consumer.py
@@ -0,0 +1,25 @@
+import boto3
+import conf as c
+import time
+
+client = boto3.client('kinesis', region_name=c.region_name)
+response = client.describe_stream(StreamName=c.stream_name)
+
+# print(response)
+
+shard_id = response['StreamDescription']['Shards'][0]['ShardId']
+shard_iterator = client.get_shard_iterator(StreamName=c.stream_name,
+                                                      ShardId=shard_id,
+                                                      ShardIteratorType='LATEST')
+
+shard_iterator = shard_iterator['ShardIterator']
+record_response = client.get_records(ShardIterator=shard_iterator,
+                                              Limit=2)
+
+while 'NextShardIterator' in record_response:
+    record_response = client.get_records(ShardIterator=record_response['NextShardIterator'],
+                                                  Limit=2)
+
+    print(record_response)
+    # wait for 5 seconds
+    time.sleep(1.5)
