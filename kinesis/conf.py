4cc636eda7d8b124075cfa9379f04685f4837ca5
Khantil Choksi <khchoksi@ncsu.edu>
2018-10-30 19:14:29 -0400

    Status Report combined

--- kinesis/conf.py
+++
@@ -1,8 +0,0 @@
-datafile='raw/weblog.csv'
-min_delay_record=0  #ms
-max_delay_record=300  #ms
-min_delay_response=0  #ms
-max_delay_response=700  #ms
-stream_name = 'LiveLog2-KinesisStream-DBEK4VO0R1HJ'
-region_name='us-west-2'
-window_size = 5
--- kinesis/setup.sh
+++
@@ -1,7 +0,0 @@
-apt-get update
-
-apt-get install python3-pip -y
-
-echo 'Installing dependencies for AWS Cloud Formation Templates'
-pip3 install boto3
-
