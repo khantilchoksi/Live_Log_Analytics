22275f8fe5101be870a0c123a4a72fd78e119211
Ubuntu <ubuntu@ip-172-31-46-203.us-west-2.compute.internal>
2018-11-17 17:34:40 +0000

    jsonify

--- datagenerator/producer.py
+++ datagenerator/producer.py
@@ -1,5 +1,6 @@
 import boto3, csv, random, time
 import sys
+import json
 sys.path.append("..")
 import conf as c
 
@@ -28,7 +29,7 @@ start_point = random.randint(0, len(clean_records)-1)
 try:
     while True:
         current_record = clean_records[start_point]
-        current_record = [str(i).encode('utf-8') for i in current_record]
+        current_record = [str(i) for i in current_record]
         # Format of data: [b'10.130.2.1', b'29/Jan/2018:20:21:57', b'home', b'302', b'3.98']
         data  = {
             'ip': current_record[0],
@@ -39,7 +40,7 @@ try:
         }
         response = client.put_record(
             StreamName = c.stream_name,
-            Data = data,
+            Data = json.dumps(data),
             PartitionKey = current_record[2]
         )
         print(f'Data: {current_record} Response: {response}')
