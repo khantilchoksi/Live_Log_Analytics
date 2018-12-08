20f2cc6003622c083eb66162868b7ded290551c8
Ubuntu <ubuntu@ip-172-31-16-97.us-west-2.compute.internal>
2018-12-08 20:58:09 +0000

    code updates

--- datagenerator/producer.py
+++ datagenerator/producer.py
@@ -27,22 +27,33 @@ for record in raw_records:
 
 start_point = random.randint(0, len(clean_records)-1)
 possible_endpoints = ['home','contest','login','profile','logout','submission']
-possible_errors = ['404','200','302','505']
-possible_range = [(100,200),(300,400), (500,700),(700,1200), (1200,1800),(400,1200)]
+possible_errors = [['404','200','302'], 
+        ['404','505'], 
+        ['302'], 
+        ['200'], 
+        ['404'], 
+        ['404','200','302','505']]
+#possible_errors = [['200','302'],
+#        ['200','505'],
+#        ['200'],
+#        ['200'],
+#        ['200'],
+#        ['200','404']]
+possible_range = [(100,200),(300,400), (500,700),(1000,1500), (1000,1400),(400,1200)]
 
 try:
     while True:
         current_record = clean_records[start_point]
         current_record = [str(i) for i in current_record]
         # Format of data: [b'10.130.2.1', b'29/Jan/2018:20:21:57', b'home', b'302', b'3.98']
-        random_enpoint_index = random.randint(0, len(possible_endpoints)-1)
+        random_endpoint_index = random.randint(0, len(possible_endpoints)-1)
 
         data  = {
             'ip': current_record[0],
             'timestamp': current_record[1],
-            'endpoint': possible_endpoints[random_enpoint_index],
-            'response_code': possible_errors[random.randint(0, len(possible_errors)-1)],
-            'response_time': random.randint(possible_range[random_enpoint_index][0], possible_range[random_enpoint_index][1])
+            'endpoint': possible_endpoints[random_endpoint_index],
+            'response_code': possible_errors[random_endpoint_index][random.randint(0, len(possible_errors[random_endpoint_index])-1)],
+            'response_time': random.randint(possible_range[random_endpoint_index][0], possible_range[random_endpoint_index][1])
         }
         response = client.put_record(
             StreamName = c.stream_name,
